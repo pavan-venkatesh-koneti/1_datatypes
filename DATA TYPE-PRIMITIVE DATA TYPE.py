edae6e284a40a28987cbdc729605aced5d71cfc7
PAVAN VENKATESH KONETI <pavankoneti85@gmail.com>
2026-06-10 15:20:50 +0530

    Add files via upload

---
+++ DATA TYPE-PRIMITIVE DATA TYPE.py
@@ -0,0 +1,6 @@
+'''primitive data types'''
+print(type(100))
+print(type(10.18))
+print(type("pavan"))
+print(type(True))
+print(type(5+6j))
---
+++ NON PRIMITIVE DATA TYPE.py
@@ -0,0 +1,20 @@
+'''non primitive data type'''
+# a=[1,2,1,3,4.45,"pavan"]
+# # print(a,type(a))
+# # print(a[3])
+# print(a[-2])
+
+# a=(1)
+# print(a,type(a))
+
+# a=(1,3,3,3.59,"pavan")
+# print(a,type(a))
+
+# a={1,3,5,3,3.56,"pavan"}
+# print(a,type(a))
+
+# a={'name':'pavan','age':20,'name':'pavan','mark':20}
+# print(a)
+
+# a={'cute':'pavan','age':20,'name':'pavan','mark':20}
+# print(a)
